3b98f35fbe1352f7d8aae0cd5e01a8024522bc22
AStudentOfCS <doduykhiem@gmail.com>
2017-11-20 16:03:48 +0700

    fix Heroku deployment

--- index.js
+++ index.js
@@ -6,7 +6,5 @@ app.get('/', (req, res) => {
 });
 
 // Dynamic Port Binding
-// Heroku tells us which port our app will use,
-// so we need to make sure we listen to the port they tell us to.
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
