4104f5aadfd1c6170a133bdc7a97b2eb1c861813
AStudentOfCS <doduykhiem@gmail.com>
2017-11-21 17:36:21 +0700

    Setting up server, Heroku deployment, Google OAuth

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
+require('./services/passport');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+require('./routes/authRoutes')(app);
 
-// Dynamic Port Binding
+// Dynamic Port Binding for Heroku deployment
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
